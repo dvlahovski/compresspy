8314c5e3fb3129c476132e53348238e8905c37e2
Dimitar Vlahovski <dvlahovski@gmail.com>
2015-07-10 01:05:06 +0300

    LZW is now implemented

--- compresspy/lzw.py
+++ compresspy/lzw.py
@@ -7,33 +7,74 @@ def open(filename, mode):
 
 class LZW():
     def __init__(self):
-        self._magic = b'\012\034' # TODO specify the magic bytes
-        self.max_dict_size = 1000 # TODO
+        self._magic = b'\012\034'  # TODO specify the magic bytes
+        self.max_dict_size = 1000  # TODO
 
     def compress(self, data):
         dictionary = dict([(bytes([i]), i) for i in range(256)])
         next_index = 256
-        output = []
+        bit_width = 9
+        codes = []
         key = bytes()
+        packed_bytes = ""
+
         for i in data:
             current_byte = bytes([i])
             if key + current_byte in dictionary:
                 key += current_byte
             else:
-                output.append(dictionary[key])
+                bin_value = bin(dictionary[key])[2:]
+                packed_bytes += '0' * (bit_width - len(bin_value)) + bin_value
                 dictionary[key + current_byte] = next_index
+                if len(bin(next_index)[2:]) > bit_width:
+                    bit_width += 1
                 next_index += 1
                 key = current_byte
 
-        output.append(dictionary[key])
-        # print(data)
-        # print(output)
-        # import operator
-        # sorted_x = sorted([i for i in dictionary.items() if i[1] >= 256 or i[1] >= 65 and i[1] <= 90] , key=operator.itemgetter(1))
-        # for i in sorted_x:
-        #     print(i)
+        bin_value = bin(dictionary[key])[2:]
+        packed_bytes += '0' * (bit_width - len(bin_value)) + bin_value
+
+        if len(packed_bytes) % 8 != 0:
+            packed_bytes += '0' * (8 - len(packed_bytes) % 8)
+
+        # pack everything into a bytearray taking 8 bits at a time
+        output = bytearray()
+        for i in range(0, len(packed_bytes), 8):
+            output.append(int(packed_bytes[i:i+8], 2))
 
         return output
 
     def decompress(self, data):
-        pass
+        dictionary = dict([(i, bytes([i])) for i in range(256)])
+        next_index = 256
+        bit_width = 9
+        position = 0
+        result = bytearray()
+
+        data_bin_str = ""
+        for i in data:
+            bin_i = bin(i)[2:]
+            data_bin_str += '0' * (8 - len(bin_i)) + bin_i
+
+        key = int(data_bin_str[position:position + bit_width], 2)
+        sequence = dictionary[key]
+        result.extend(sequence)
+        position += bit_width
+
+        while position + bit_width <= len(data_bin_str):
+            key = int(data_bin_str[position:position + bit_width], 2)
+            if key in dictionary:
+                current = dictionary[key]
+            elif key == next_index:
+                current = sequence + bytes([sequence[0]])
+
+            result.extend(current)
+            dictionary[next_index] = sequence + bytes([current[0]])
+            next_index += 1
+            position += bit_width
+            if len(bin(next_index)[2:]) > bit_width:
+                bit_width += 1
+
+            sequence = current
+
+        return result
--- tests/test_lzw.py
+++ tests/test_lzw.py
@@ -1,18 +1,36 @@
 import unittest
 from compresspy import lzw
+from random import randint
+
 
 class TestLZW(unittest.TestCase):
     def setUp(self):
         self.compressor = lzw.LZW()
+        # TOBEORNOTTOBEORTOBEORNOT
 
     def test(self):
-        data = "TOBEORNOTTOBEORTOBEORNOT"
+        data = ""
+        for i in range(10**6):
+            data += chr(randint(97, 122))
         data_bytes = bytes(data, "utf-8")
         compressed_data = self.compressor.compress(data_bytes)
-        self.assertEqual(len(compressed_data), 16)
-        known_output = [84, 79, 66, 69, 79, 82, 78, 79, 84, 256, 258, 260, 265, 259, 261, 263]
-        self.assertEqual(compressed_data, known_output)
+        # self.assertEqual(len(compressed_data), 16)
+        # known_output = [84, 79, 66, 69, 79, 82, 78, 79, 84, 256, 258, 260,
+        #                 265, 259, 261, 263]
+        # self.assertEqual(compressed_data, known_output)
+        # print(compressed_data)
+        print(len(compressed_data))
+        print((1 - len(compressed_data) / len(data))*100)
 
+        self.assertEqual(data, self.compressor.decompress(compressed_data)
+            .decode("utf-8"))
+
+    def test_corner_case_in_LZW(self):
+        data = "ASDASDASDASDASDASDASDASDASD"
+        data_bytes = bytes(data, "utf-8")
+        compressed_data = self.compressor.compress(data_bytes)
+        self.assertEqual(data, self.compressor.decompress(compressed_data)
+            .decode("utf-8"))
 
 if __name__ == '__main__':
     unittest.main()
