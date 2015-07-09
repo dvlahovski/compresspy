19d6c8d96265c5758a04122be9435a5510cb5485
Dimitar Vlahovski <dvlahovski@gmail.com>
2015-07-10 01:23:22 +0300

    Fixed tests

--- tests/test_lzw.py
+++ tests/test_lzw.py
@@ -6,31 +6,32 @@ from random import randint
 class TestLZW(unittest.TestCase):
     def setUp(self):
         self.compressor = lzw.LZW()
-        # TOBEORNOTTOBEORTOBEORNOT
 
-    def test(self):
-        data = ""
-        for i in range(10**6):
-            data += chr(randint(97, 122))
+    def compress(self, data):
         data_bytes = bytes(data, "utf-8")
-        compressed_data = self.compressor.compress(data_bytes)
-        # self.assertEqual(len(compressed_data), 16)
-        # known_output = [84, 79, 66, 69, 79, 82, 78, 79, 84, 256, 258, 260,
-        #                 265, 259, 261, 263]
-        # self.assertEqual(compressed_data, known_output)
-        # print(compressed_data)
-        print(len(compressed_data))
-        print((1 - len(compressed_data) / len(data))*100)
+        return self.compressor.compress(data_bytes)
 
-        self.assertEqual(data, self.compressor.decompress(compressed_data)
-            .decode("utf-8"))
+    def decompress(self, data):
+        return self.compressor.decompress(data).decode("utf-8")
+
+    def compare(self, data):
+        compressed_data = self.compress(data)
+        decompressed_data = self.decompress(compressed_data)
+        self.assertEqual(data, decompressed_data)
+
+    def test_wikipedia_example(self):
+        data = "TOBEORNOTTOBEORTOBEORNOT"
+        self.compare(data)
+
+    def test_random_lowercase_letters(self):
+        data = ""
+        for i in range(10**4):
+            data += chr(randint(97, 122))
+        self.compare(data)
 
     def test_corner_case_in_LZW(self):
         data = "ASDASDASDASDASDASDASDASDASD"
-        data_bytes = bytes(data, "utf-8")
-        compressed_data = self.compressor.compress(data_bytes)
-        self.assertEqual(data, self.compressor.decompress(compressed_data)
-            .decode("utf-8"))
+        self.compare(data)
 
 if __name__ == '__main__':
     unittest.main()
